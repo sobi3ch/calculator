f49402db8ce959931fa7effe0dd6751c48ea80a9
sobi3ch <sobiech@bohater.net>
2013-11-23 18:50:30 +0000

    Adding main class with first feature with PHPUnit test

---
+++ test/calculator_test.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'calculator.php';
+
+class CalculatorTest extends PHPUnit_Framework_TestCase 
+{
+	public function testAdd() {
+		$c = new Calculator;
+		$result = $c->add(5, 10);
+		$this->assertEquals(15, $result);
+	}
+}
